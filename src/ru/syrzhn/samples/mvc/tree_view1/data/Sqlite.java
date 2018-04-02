d8e593ce6c4fbc0d5347a92c56884a6813ed30c8
syrzhn <Pinzhenin.Sergey@gmail.com>
2018-04-02 16:21:47 +0500

    Sqlite is working

--- src/ru/syrzhn/samples/mvc/tree_view1/data/Sqlite.java
+++ src/ru/syrzhn/samples/mvc/tree_view1/data/Sqlite.java
@@ -2,7 +2,9 @@ package ru.syrzhn.samples.mvc.tree_view1.data;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,53 +17,78 @@ import org.w3c.dom.Element;
 import ru.syrzhn.samples.mvc.tree_view1.model.XmlUtils;
 
 public class Sqlite {
-	
-	 /** Connect to a sample database */
-    public static void connect() {
-        Connection conn = null;
-        try {
-            // db parameters
-            String url = "jdbc:sqlite:src/ru/syrzhn/samples/mvc/tree_view1/data/chinook.db";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-            
-            System.out.println("Connection to SQLite has been established.");
-            
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
-
 	public Stack<String> messages;
-	public int progress;
-	
-	public Sqlite[] mChildren;
-
+	public int progress;	
+	public Stack<Sqlite> mChildren;
 	public List<Map<String, String>> mXmlNodeData;
 	public Document mDoc;
 	
+	/** Connect to a database */
+	public void connect(String dbName) {
+		// load the sqlite-JDBC driver using the current class loader
+		try { // it is not necessary in this version of lib - "sqlite-jdbc-3.21.0.jar"
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) { e.printStackTrace(); }
+		Connection conn = null;
+		try {
+			// db parameters
+			String url = "jdbc:sqlite:" + dbName;
+			// create a connection to the database
+			conn = DriverManager.getConnection(url);
+			Statement statement = conn.createStatement();
+			statement.setQueryTimeout(30);  // set timeout to 30 sec.
+			ResultSet resultSet = statement.executeQuery("select * from sqlite_master where type = 'table'");
+			while (resultSet.next()) { // iterate & read the result set
+				mChildren.add(new Sqlite(resultSet));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
 	public Sqlite(Object data) {
-		String name = "!!!EMPTYNESS!!!", ID = name;
-		// TODO Auto-generated constructor stub
 		mXmlNodeData = new ArrayList<Map<String, String>>();
-		HashMap<String, String> tagID = new HashMap<String, String>();
-		tagID.put("tagName", "ID");
-		tagID.put("tagValue", ID);
-		tagID.put("tagType", "text");
-		mXmlNodeData.add(tagID);
-		HashMap<String, String> tagName = new HashMap<String, String>();
-		tagName.put("tagName", "name");
-		tagName.put("tagValue", name);
-		tagName.put("tagType", "text");
-		mXmlNodeData.add(tagName);
+		mChildren    = new Stack<Sqlite>();
+		if (data instanceof String) {
+			String name = data.toString();
+			HashMap<String, String> tagID = new HashMap<String, String>();
+			tagID.put("tagName", "database_name");
+			tagID.put("tagValue", "chinook.db");
+			tagID.put("tagType", "text");
+			mXmlNodeData.add(tagID);
+			HashMap<String, String> tagName = new HashMap<String, String>();
+			tagName.put("tagName", "database_path");
+			tagName.put("tagValue", name);
+			tagName.put("tagType", "text");
+			mXmlNodeData.add(tagName);
+		} else if (data instanceof ResultSet) {
+			ResultSet resultSet = (ResultSet)data;
+			try {
+				HashMap<String, String> tagTabName = new HashMap<String, String>();
+				tagTabName.put("tagName", "table_name");
+				tagTabName.put("tagValue", resultSet.getString("name"));
+				tagTabName.put("tagType", "text");
+				tagTabName.put("tbl_name", resultSet.getString("tbl_name"));
+				tagTabName.put("rootpage", resultSet.getInt("rootpage") + "");
+				tagTabName.put("sql", resultSet.getString("sql"));
+				mXmlNodeData.add(tagTabName);
+				HashMap<String, String> tagTabType = new HashMap<String, String>();
+				tagTabType.put("tagName", "table_type");
+				tagTabType.put("tagValue", resultSet.getString("type"));
+				tagTabType.put("tagType", "text");
+				mXmlNodeData.add(tagTabType);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	private Element createRow(final String row_number) {
@@ -80,19 +107,16 @@ public class Sqlite {
 	}
 
 	public Document getDocument() {
-		connect();
+		connect("src/ru/syrzhn/samples/mvc/tree_view1/data/chinook.db");
 		mDoc = XmlUtils.createXmlDocument();
 		Element root = mDoc.getDocumentElement();
 		Element fstEl = createRow("main row");
+		fstEl.setAttribute("tree_actions", "select");
 		root.appendChild(fstEl);
-		Object[] children = null;
-		// TODO Auto-generated method stub
-		int size = children.length;
-		mChildren = new Sqlite[size];
+		int size = mChildren.size();
 		for (int i = 0; i < size; i++) {
-			mChildren[i] = new Sqlite(children[i]);
-			mChildren[i].mDoc = mDoc;
-			Element rowI = mChildren[i].createRow(i + "");
+			mChildren.get(i).mDoc = mDoc;
+			Element rowI = mChildren.get(i).createRow(i + "");
 			fstEl.appendChild(rowI);
 		}
 		XmlUtils.saveToFile(mDoc, "src\\ru\\syrzhn\\samples\\mvc\\tree_view1\\xml\\output.xml");
